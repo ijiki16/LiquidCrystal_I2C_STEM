328dd6e1b8f18c164505af5b7ccfb742c2dabcfe
iuri <ijiki16@freeuni.edu.ge>
2024-03-20 09:30:25 +0400

    update hello word example

--- examples/HelloWorld/HelloWorld.ino
+++ examples/HelloWorld/HelloWorld.ino
@@ -1,19 +1,16 @@
 #include <LiquidCrystal_I2C_STEM.h>
 
-LiquidCrystal_I2C_STEM lcd(0x27, 20, 4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C_STEM lcd(0x27, 16, 2);  // set the LCD address to 0x27 for a 20 chars and 4 line display
 
 void setup() {
-    lcd.init();  // initialize the lcd
+    lcd.init();       // initialize the lcd
+    lcd.backlight();  // Set backlight on
+
     // Print a message to the LCD.
-    lcd.backlight();
-    lcd.setCursor(3, 0);
+    lcd.setCursor(2, 0);
     lcd.print("Hello, world!");
-    lcd.setCursor(2, 1);
-    lcd.print("Ywrobot Arduino!");
-    lcd.setCursor(0, 2);
-    lcd.print("Arduino LCM IIC 2004");
-    lcd.setCursor(2, 3);
-    lcd.print("Power By Ec-yuan!");
+    lcd.setCursor(0, 1);
+    lcd.print("Arduino Display!");
 }
 
 void loop() {
